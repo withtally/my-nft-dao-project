afa5df524daea94bf0807bb701116bc8c2f5bff0
Chris Rusyniak <c.rusyn@gmail.com>
2021-10-27 17:16:49 +0100

    chore(deploy): cleanup imports

--- tasks/deploy/dao.ts
+++ tasks/deploy/dao.ts
@@ -1,4 +1,4 @@
-import { subtask, task } from "hardhat/config";
+import { task } from "hardhat/config";
 import { getExpectedContractAddress } from "../utils";
 
 import {
@@ -35,7 +35,7 @@ task("deploy:Dao").setAction(async function (_, { ethers, run }) {
     governorExpectedAddress,
     governor: governor.address,
     timelock: timelock.address,
-    token: token.address
+    token: token.address,
   });
 
   // We'll mint enough NFTs to be able to pass a proposal!
@@ -43,30 +43,24 @@ task("deploy:Dao").setAction(async function (_, { ethers, run }) {
   await token.safeMint(signerAddress);
   await token.safeMint(signerAddress);
 
-  console.log("Minted 3 NFTs to get us started")
+  console.log("Minted 3 NFTs to get us started");
 
   // Allow the timelock to perform actions on the NFT contract as part of proposal execution
   await token.setApprovalForAll(timelock.address, true);
 
-  console.log("Granted the timelock permissions to run safeMint()")
+  console.log("Granted the timelock permissions to run safeMint()");
 
   await run("verify:verify", {
     address: token.address,
-  })
-  
+  });
+
   await run("verify:verify", {
     address: timelock.address,
-    constructorArguments: [
-      governor.address,
-      timelockDelay
-    ]
-  })
+    constructorArguments: [governor.address, timelockDelay],
+  });
 
   await run("verify:verify", {
     address: governor.address,
-    constructorArguments: [
-      token.address,
-      timelock.address
-    ]
-  })
-})
+    constructorArguments: [token.address, timelock.address],
+  });
+});
