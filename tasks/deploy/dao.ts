05361a5670aa7a2061f117c75cb784c4425256f5
Chris Rusyniak <c.rusyn@gmail.com>
2021-10-27 17:08:59 +0100

    feat(deploy): mint and verify

--- tasks/deploy/dao.ts
+++ tasks/deploy/dao.ts
@@ -1,4 +1,4 @@
-import { task } from "hardhat/config";
+import { subtask, task } from "hardhat/config";
 import { getExpectedContractAddress } from "../utils";
 
 import {
@@ -10,7 +10,7 @@ import {
   Timelock__factory,
 } from "../../typechain";
 
-task("deploy:Dao").setAction(async function (_, { ethers }) {
+task("deploy:Dao").setAction(async function (_, { ethers, run }) {
   const timelockDelay = 2;
 
   const tokenFactory: MyNftToken__factory = await ethers.getContractFactory("MyNftToken");
@@ -35,6 +35,38 @@ task("deploy:Dao").setAction(async function (_, { ethers }) {
     governorExpectedAddress,
     governor: governor.address,
     timelock: timelock.address,
-    token: token.address,
+    token: token.address
   });
-});
+
+  // We'll mint enough NFTs to be able to pass a proposal!
+  await token.safeMint(signerAddress);
+  await token.safeMint(signerAddress);
+  await token.safeMint(signerAddress);
+
+  console.log("Minted 3 NFTs to get us started")
+
+  // Allow the timelock to perform actions on the NFT contract as part of proposal execution
+  await token.setApprovalForAll(timelock.address, true);
+
+  console.log("Granted the timelock permissions to run safeMint()")
+
+  await run("verify:verify", {
+    address: token.address,
+  })
+  
+  await run("verify:verify", {
+    address: timelock.address,
+    constructorArguments: [
+      governor.address,
+      timelockDelay
+    ]
+  })
+
+  await run("verify:verify", {
+    address: governor.address,
+    constructorArguments: [
+      token.address,
+      timelock.address
+    ]
+  })
+})
